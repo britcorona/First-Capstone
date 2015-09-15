c160b8919dac4575f87ffdd1254d1e031d07ea7e
Brittney Corona <BrittneyLCorona@gmail.com>
2015-09-15 11:51:05 -0500

    prices page working

--- app/app.js
+++ app/app.js
@@ -2,16 +2,19 @@ define([
 	'angular',
 	'angularRoute',
 	'angularFire',
+	'jquery',
 	'controllers/firstPageCtrl',
 	'controllers/pricesPageCtrl',
-	'controllers/checklistPageCtrl'
-], function(angular, angularRoute, angularFire, firstCtrl, pricesCtrl, checklistCtrl) {
+	'controllers/checklistPageCtrl',
+	'controllers/displayInfoCtrl'
+], function(angular, angularRoute, angularFire, jquery, firstCtrl, pricesCtrl, checklistCtrl, infoCtrl) {
 	return angular.module("CapstoneApp", [
 		'ngRoute',
 		'firebase',
 		'CapstoneApp.FirstPageCtrl',
 		'CapstoneApp.PricesPageCtrl',
-		'CapstoneApp.ChecklistPageCtrl'
+		'CapstoneApp.ChecklistPageCtrl',
+		'CapstoneApp.displayInfoCtrl'
 	]).
 
 config(['$routeProvider', function($routeProvider) {
--- app/controllers/displayInfoCtrl.js
+++ app/controllers/displayInfoCtrl.js
@@ -3,22 +3,24 @@ define([
 	'firebase',
 	'angularRoute'
 ], function(angular, firebase, angularRoute) {
-	angular.module("CapstoneApp.PricesPageCtrl", ['ngRoute'])
+	angular.module("CapstoneApp.displayInfoCtrl", ['ngRoute'])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider
-			.when('/prices', {
-				templateUrl: '../partials/prices-page.html',
-				controller: 'pricesCtrl',
-				controllerAs: 'PricesPageCtrl'
+			.when('/info', {
+				templateUrl: '../partials/info.html',
+				controller: 'infoCtrl',
+				controllerAs: 'displayInfoCtrl'
 			});
 		}])
-		.controller("pricesCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+		.controller("infoCtrl", ["$scope", "$firebaseArray", "$routeParams", function($scope, $firebaseArray, $routeParams) {
 
 		 //This will connect to firebase and get the info
 		  var ref = new Firebase("https://first-capstone.firebaseio.com/vehicles");
 
 		 // download the data into a local object
 		  $scope.vehicles = $firebaseArray(ref);
+
+		  $scope.id = $routeParams.songId;
 
 	}]);
 });
